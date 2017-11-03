efdcb802b8707f936470f50a761f2a836542e6f6
David Benton <soccershelbyb15@gmail.com>
2017-11-03 19:15:26 -0400

    initialise 2

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'WMeeR7lv6GJNSCbDesmg5Mrru',
+  consumer_secret: 'ajrLk5YdSNd2431cIf67ZGbzIRgkQcOpy5UVUYNFYvUoUFEMU4',
+  access_token_key: '1692001388-G157CSCUI3r6ngS31GfPBJnMSa84tXXpT1hQfKr',
+  access_token_secret: 'wcfn8w1ujOGHvFSS0KjupS7eN8nkzaUfTJOr3iGgXJrCC',
+}
+
+var spotifyKeys = {
+    client_ID: 'eb2bfb6ed0414930b9c1b89476f85e5b'
+    client_secret: 'e09144d0426c4c5fa29eb92db14c15c8'
+}
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,18 @@
+// requirements
+
+// input variables from terminal
+
+// if statement runs through process.argv[2] 
+// tihs determines what module to use
+
+// 1. 'my-tweets' displays: last 20 tweets
+
+// 2. 'spotify-this-song' displays; artist, song, link, album
+// if no song default to 'The Sign'
+
+// 3. 'movie-this' diplays: title, release year, imdb rating,
+// rotten tomatoes rating, country produced, language, plot, actors
+// default move is 'Mr. Nobody'
+// NOTE: you'll have to use 'Request' use API key '40e9cece'
+
+// 4. 'do-what-it-says' take the text in random.txt file and run it
